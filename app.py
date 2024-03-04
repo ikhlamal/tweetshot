b218bc08b15d6504b05407196ac48fe611dcfd96
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2024-03-04 23:39:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import subprocess
 import streamlit as st
 
-# URL tweet yang ingin diambil screenshotnya
-tweet_url = "https://twitter.com/hideo_kojima_en/status/1002107372091817984"
-
-# Menjalankan tweetshot menggunakan subprocess
+# Mengambil screenshot dari tweet
+tweet_url = "https://twitter.com/jack/status/20"
 try:
-    subprocess.run(["tweetshot", tweet_url], check=True)
-    st.write("Screenshot berhasil diambil!")
+    subprocess.run(["tweetcapture", tweet_url], check=True)
+    print("Screenshot tweet berhasil diambil!")
 except subprocess.CalledProcessError as e:
     print("Error:", e)
